f96e74d1f221e9bba5638ce6200e38ee8fb9c857
peerapat <peerapatza401@gmail.com>
2024-01-15 10:53:05 +0700

    calculator in function component
    calculator in function component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Header, Content, Footer } from "./func-components";
 import Button from "./class-components";
+import { Calculator2 } from "./calculator";
 
 function App() {
-  return <Button />;
+  return <Calculator2/>
 }
 
 export default App;
