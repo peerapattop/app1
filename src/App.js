427e10a692468fae648a6f0829941e4bd93f4488
peerapat <peerapatza401@gmail.com>
2024-01-15 11:34:02 +0700

    EventData2 in function component
    EventData2 in function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { Header, Content, Footer } from "./func-components";
-import Button from "./class-components";
-import { Calculator2 } from "./calculator";
+import { EventData2 } from "./event-data";
 
-function App() {
-  return <Calculator2/>
+export default function App() {
+  return <EventData2/>
 }
-
-export default App;
